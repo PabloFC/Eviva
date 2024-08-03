d3508419042567f7048219ea7bea505db5c35127
PabloFC <pablofc_68@hotmail.com>
2024-08-03 22:40:24 +0200

    hero mobile responsive done

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,43 +1,8 @@
-import hero from "../assets/img/hero.png";
-
 const Hero = () => {
   return (
-    <section
-      style={{
-        backgroundImage: `url(${hero})`,
-        backgroundSize: "cover",
-        // backgroundPosition: "center",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      {/* <div className="w-[650px] h-[100vh] ml-32 flex items-center ">
-        <div className=" bg-white opacity-[30%] mt-24 xl:h-[70%] rounded-xl">
-          <h3 className="text-center uppercase font-bold text-2xl p-4 2xl:p-20 ">
-            You feel tired, hungry, foggy, anxious, and you crave for sugar all
-            the time?
-          </h3>
-          <div className="p-2">
-            <p className="text-center uppercase">JOIN LIVE</p>
-            <p className="text-center uppercase">
-              WOMEN’S HORMONAL HEALTH WEBINAR
-            </p>
-          </div>
-          <h2 className="text-center font-bold text-4xl uppercase p-4 2xl:p-6">
-            Boost Your Mood & Get Back Your Energy
-          </h2>
-          <p className="text-center uppercase p-4 2xl:p-6">
-            10th september 2024 12 PM - 1 PM CET
-          </p>
-          <div className="flex justify-center p-4 2xl:p-6">
-            <a className="text-center uppercase custom-button ">
-              Save your spot
-            </a>
-          </div>
-        </div>
-      </div> */}
-      <div className="w-[650px] h-[100vh] ml-32 flex items-center">
-        <div className="bg-white bg-opacity-30 mt-24 xl:h-[70%] rounded-xl">
+    <section className="hero">
+      <div className="w-[343px] h-[630px] mx-auto lg:w-[650px] lg:h-[100vh] xl:ml-32 flex items-center">
+        <div className="bg-white bg-opacity-30 mt-48 xl:mt-32 xl:h-[70%] rounded-xl">
           <h3 className="text-center uppercase font-bold text-2xl p-4 2xl:p-20">
             You feel tired, hungry, foggy, anxious, and you crave for sugar all
             the time?
@@ -55,7 +20,7 @@ const Hero = () => {
             10th september 2024 12 PM - 1 PM CET
           </p>
           <div className="flex justify-center p-4 2xl:p-6">
-            <a className="text-center uppercase custom-button">
+            <a className="text-center uppercase custom-button text-white text-sm font-bold px-4 py-2.5 bg-[#a6035d] rounded-full cursor-pointer">
               Save your spot
             </a>
           </div>
